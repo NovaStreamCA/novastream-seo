b25d21f2a9cbee0c1a461ea82326c37077beab5b
GeorgeDylan <16292628+GeorgeDylan@users.noreply.github.com>
2022-08-05 14:31:28 +0000

    Update Version

--- seo.php
+++ seo.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Novastream SEO
- * @version 1.2.2
+ * @version 1.0.1
  */
 /*
 Plugin Name: Novastream SEO
 Plugin URI: https://novastream.ca
 Description: NovaStream's SEO Plugin
 Author: NovaStream
-Version: 1.0.0
+Version: 1.0.1
 Author URI: https://novastream.ca
 */
 
