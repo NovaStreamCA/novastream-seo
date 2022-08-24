a6086240b2f75427e3a566bd38f0c56c0ddc75e1
Matthew Lewis <9137721+program-the-brain-not-the-heartbeat@users.noreply.github.com>
2022-08-24 15:50:30 -0300

    fix version number

--- seo.php
+++ seo.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Novastream SEO
- * @version 1.0.1
+ * @version 1.0.2
  */
 /*
 Plugin Name: Novastream SEO
 Plugin URI: https://novastream.ca
 Description: NovaStream's SEO Plugin
 Author: NovaStream
-Version: 1.0.1
+Version: 1.0.2
 Author URI: https://novastream.ca
 */
 
