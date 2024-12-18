1c968f17235ba482dba05fcbb2fad75cd9fccfdc
GeorgeDylan <claymoarplays@gmail.com>
2024-12-18 15:58:41 +0000

    v

--- seo.php
+++ seo.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Novastream SEO
- * @version 1.1.2
+ * @version 1.1.3
  */
 /*
 Plugin Name: Novastream SEO
 Plugin URI: https://novastream.ca
 Description: NovaStream's SEO Plugin
 Author: NovaStream
-Version: 1.1.2
+Version: 1.1.3
 Author URI: https://novastream.ca
 */
 
