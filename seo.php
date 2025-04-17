86e894f73b49cbe49856285c17268985f8fb35d7
GeorgeDylan <claymoarplays@gmail.com>
2025-04-17 12:41:05 +0000

    Fix SEO init bug

--- seo.php
+++ seo.php
@@ -77,12 +77,11 @@ if(!class_exists("NovaStreamSEO"))
     } // END class NovaStreamSEO
 } // END if(!class_exists("NovaStreamSEO"))
 
-if(class_exists('NovaStreamSEO')) {
-
-    // instantiate the plugin class
-    $plugin = new NovaStreamSEO();
-
-} // END if(class_exists('NovaStreamSEO'))
+add_action('init', function() {
+    if(class_exists('NovaStreamSEO')) {
+        new NovaStreamSEO();
+    }
+});
 
 // ACF to show all post types as checkbox
 function acf_load_post_types($field) {
